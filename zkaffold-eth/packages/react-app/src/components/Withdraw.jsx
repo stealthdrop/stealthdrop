9f52599920e9cfee9f1b56feb3a9e271a0626dc8
Adhyyan Sekhsaria <adhyyan1252@gmail.com>
2022-01-15 16:41:47 -0800

    stuff

--- zkaffold-eth/packages/react-app/src/components/Withdraw.jsx
+++ zkaffold-eth/packages/react-app/src/components/Withdraw.jsx
@@ -25,18 +25,18 @@ const exampleProof = [
   [8, 9, 10, 11],
 ];
 
-async function postData(url, data) {
-    const requestOptions = {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    };
-    const response = await fetch(url, requestOptions);
-    console.log("response", response);
-    const jsres = await response.json();
-    console.log("jsres", jsres);
-    return jsres;
+async function postData(url = "", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: [["Content-Type", "application/json"]],
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response; // parses JSON response into native JavaScript objects
 }
 
 export default function Withdraw({ signer, address, web3Modal, loadWeb3Modal, mainnetProvider, provider }) {
