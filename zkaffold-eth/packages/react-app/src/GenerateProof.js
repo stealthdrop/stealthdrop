44b7a85f6353337acafbbe6ce6ddc535895341f3
Aayush G <aayushg@mit.edu>
2022-02-07 00:49:19 -0500

    fix indenting and change mimchash key to 123

--- zkaffold-eth/packages/react-app/src/GenerateProof.js
+++ zkaffold-eth/packages/react-app/src/GenerateProof.js
@@ -1,14 +1,13 @@
 /* global BigInt */
 import { getPath, merkleTreeRoot } from "./AirdropData";
 import { mimcHash } from "./mimc";
-import bigInt from 'big-integer';
+import bigInt from "big-integer";
 import { ethers } from "ethers";
 
-const fromHexString = (hexString) =>
-  new Uint8Array(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
+const fromHexString = hexString => new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
 
-const intToHex = (intString) => ethers.BigNumber.from(intString).toHexString();
-const hexStringTobigInt = (hexString) => {
+const intToHex = intString => ethers.BigNumber.from(intString).toHexString();
+const hexStringTobigInt = hexString => {
   return Uint8Array_to_bigint(fromHexString(hexString));
 };
 
@@ -33,72 +32,71 @@ function Uint8Array_to_bigint(x) {
 }
 
 function bigint_to_tuple(x) {
-    // 2 ** 86
-    x = BigInt(x);
-    let mod = 77371252455336267181195264n;
-    let ret = [0n, 0n, 0n];
+  // 2 ** 86
+  x = BigInt(x);
+  let mod = 77371252455336267181195264n;
+  let ret = [0n, 0n, 0n];
 
-    var x_temp = x;
-    for (var idx = 0; idx < 3; idx++) {
-        ret[idx] = x_temp % mod;
-        x_temp = x_temp / mod;
-    }
-    return ret;
+  var x_temp = x;
+  for (var idx = 0; idx < 3; idx++) {
+    ret[idx] = x_temp % mod;
+    x_temp = x_temp / mod;
+  }
+  return ret;
 }
 
 function bigint_to_array(n, k, x) {
-    let mod = 1n;
-    for (var idx = 0; idx < n; idx++) {
-        mod = mod * 2n;
-    }
+  let mod = 1n;
+  for (var idx = 0; idx < n; idx++) {
+    mod = mod * 2n;
+  }
 
-    let ret = [];
-    var x_temp = x;
-    for (var idx = 0; idx < k; idx++) {
-        ret.push(x_temp % mod);
-        x_temp = x_temp / mod;
-    }
-    return ret;
+  let ret = [];
+  var x_temp = x;
+  for (var idx = 0; idx < k; idx++) {
+    ret.push(x_temp % mod);
+    x_temp = x_temp / mod;
+  }
+  return ret;
 }
 
 function parseSignature(sig) {
-    console.log("sig", sig);
-    const r_hex = sig.slice(2, 66)
-    const s_hex = sig.slice(66, 66+64)
-    // console.log("sig stuff", sig_arr.length, sig_arr);
-    var r_bigint = hexStringTobigInt(r_hex);
-    var s_bigint = hexStringTobigInt(s_hex);
-    var r_array = bigint_to_array(86, 3, r_bigint);
-    var s_array = bigint_to_array(86, 3, s_bigint);
-    console.log("s_bigint", s_bigint);
-    return [r_array, s_array];
+  console.log("sig", sig);
+  const r_hex = sig.slice(2, 66);
+  const s_hex = sig.slice(66, 66 + 64);
+  // console.log("sig stuff", sig_arr.length, sig_arr);
+  var r_bigint = hexStringTobigInt(r_hex);
+  var s_bigint = hexStringTobigInt(s_hex);
+  var r_array = bigint_to_array(86, 3, r_bigint);
+  var s_array = bigint_to_array(86, 3, s_bigint);
+  console.log("s_bigint", s_bigint);
+  return [r_array, s_array];
 }
 
 function parsePubkey(pk) {
-    const sliced_pk = pk.slice(4);
-    const pk_x_hex = sliced_pk.slice(0, 64);
-    const pk_y_hex = sliced_pk.slice(64, 128);
-    const pk_x_bigint = hexStringTobigInt(pk_x_hex);
-    const pk_y_bigint = hexStringTobigInt(pk_y_hex);
-    const pk_x_arr = bigint_to_array(86, 3, pk_x_bigint);
-    const pk_y_arr = bigint_to_array(86, 3, pk_y_bigint);
-    console.log("pk stuff", pk, pk_x_arr, pk_y_arr);
-    return [ pk_x_arr, pk_y_arr ];
+  const sliced_pk = pk.slice(4);
+  const pk_x_hex = sliced_pk.slice(0, 64);
+  const pk_y_hex = sliced_pk.slice(64, 128);
+  const pk_x_bigint = hexStringTobigInt(pk_x_hex);
+  const pk_y_bigint = hexStringTobigInt(pk_y_hex);
+  const pk_x_arr = bigint_to_array(86, 3, pk_x_bigint);
+  const pk_y_arr = bigint_to_array(86, 3, pk_y_bigint);
+  console.log("pk stuff", pk, pk_x_arr, pk_y_arr);
+  return [pk_x_arr, pk_y_arr];
 }
-
 
 export function generateProofInputs(address, signature, proverPubkey, claimerAddress, msghash) {
   const val = getPath(address);
-  if(!val) return null;
+  if (!val) return null;
   const [root, pathElements, pathIndices] = val;
   console.log("signature", signature);
-  const [ r_array, s_array ] = parseSignature(signature);
-  const [ pubkey_x, pubkey_y ] = parsePubkey(proverPubkey);
+  const [r_array, s_array] = parseSignature(signature);
+  const [pubkey_x, pubkey_y] = parsePubkey(proverPubkey);
 
   const msghash_bigint = hexStringTobigInt(msghash);
   var msghash_array = bigint_to_array(86, 3, msghash_bigint);
 
-  const nullifierHash = mimcHash(1)(r_array[0], r_array[1], r_array[2], s_array[0], s_array[1], s_array[2]);
+  const nullifierHash = mimcHash(123)(r_array[0], r_array[1], r_array[2], s_array[0], s_array[1], s_array[2]);
 
   const input = {
     root: root.toString(),
